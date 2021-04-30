841e16ff7c5e1d0db38242def0f3d3745d89e4a1
prathit-web <73165764+prathit-web@users.noreply.github.com>
2021-04-30 10:32:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function get_para1()
+{
+var inpits=[];
+
+
+
+}
+for(var i=1;i <=6; i++)
+{
+inpits.push(document.getElementById("p_1"+ i).value);
+}
+   inpits.join(".") ;
+    
+document.getElementById("showpara1").innerHTML=inputs.join(".");
